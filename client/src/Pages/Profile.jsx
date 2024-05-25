68dd5c8a9402be3221a1eb9475c69a84c4d3ad9c
saranedubridge <saranraj.1803164@srec.ac.in>
2024-05-25 14:27:54 +0530

    added signout functionlity

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -7,7 +7,7 @@ import{
    uploadBytesResumable
   } from 'firebase/storage';
 import { app } from '../firebase';
-import { UpdateUserStart,UpdateUserSuccess,UpdateUserFailure, deleteUserFaliure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { UpdateUserStart,UpdateUserSuccess,UpdateUserFailure, deleteUserFaliure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFaliure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 
@@ -112,6 +112,29 @@ export default function Profile() {
         }
       }
 
+
+      const handleSignOut=async()=>{
+        try{
+          dispatch(signOutUserStart())
+          const res = await fetch('api/auth/signout');
+          const data = await res.json();
+          if(data.success===false){
+            dispatch (signOutUserFaliure(data.message))
+            return;
+          }
+          dispatch(signOutUserSuccess(data));
+
+
+        }catch(error){
+          dispatch(signOutUserFaliure(data.message))
+
+        }
+      }
+
+
+
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -173,7 +196,7 @@ export default function Profile() {
 
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
        <p className='text-red-700 mt-5'> {error?error:''}</p>
        <p className='text-green-700 mt-5'>{updateSuccess? 'User is Updated successfully!':""}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -53,6 +53,22 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
+        
+        
+        signOutUserStart:(state)=>{
+            state.loading=true;
+        },
+
+        signOutUserSuccess:(state)=>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+
+        signOutUserFaliure:(state,action)=>{
+            state.error = action.payload;
+            state.loading = false;
+        },
 
     },
       
@@ -68,5 +84,8 @@ export const {
     deleteUserFaliure,
     deleteUserStart,
     deleteUserSuccess,
+    signOutUserFaliure,
+    signOutUserStart,
+    signOutUserSuccess,
 }=userSlice.actions;
 export default userSlice.reducer;
