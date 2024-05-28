149e48c4351a48d072d4e056d705bc51ec9496f9
saranedubridge <saranraj.1803164@srec.ac.in>
2024-05-28 23:41:38 +0530

    create get user lisiting api routers

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test,UpdateUser,deleteUser } from '../controllers/user.controller.js';
+import { test,UpdateUser,deleteUser,getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router=express.Router();
@@ -7,6 +7,7 @@ const router=express.Router();
 router.get('/test',test);
 router.post('/update/:id', verifyToken,UpdateUser)
 router.delete('/delete/:id', verifyToken,deleteUser)
+router.get('/listings/:id', verifyToken,getUserListings)
 
 
 export default router;
