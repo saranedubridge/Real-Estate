70de00c29aecd347eda30fee4eecd730488fadeb
saranedubridge <saranraj.1803164@srec.ac.in>
2024-06-20 19:31:00 +0530

    create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRouter from "./Components/PrivateRouter";
 import CreateListing from "./Pages/CreateListing";
 import UpdateListing from "./Pages/UpdateListing";
 import Listing from "./Pages/Listing";
+import Search from "./Pages/Search";
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
   <Route path="/sign-in" element={<SignIn/>}/>
   <Route path="/sign-up" element={<SignUp/>}/>
   <Route path="/about" element={<About/>}/>
+  <Route path="/search"element={<Search/>}/>
   <Route path="/listing/:listingId" element={<Listing/>}/>
   <Route element={<PrivateRouter/>}>
     <Route path='/profile' element={<Profile/>}/>
